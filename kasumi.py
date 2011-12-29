eed159ff82e9c3d119625010c2f9ecd71c3d606a
Bo Zhu <zhubo03@gmail.com>
2011-12-29 18:13:25 -0500

    need to finish FI FO FL

--- kasumi.py
+++ kasumi.py
@@ -5,6 +5,15 @@ def _bitlen(x):
     return len(bin(x)) - 2
 
 
+def _rol16(x, s):
+    assert _bitlen(x) <= 16
+    return ((x << s) & 0xFFFF) | (x >> (16 - s))
+
+
+def _mod(x):
+    return ((x - 1) % 8) + 1
+
+
 class Kasumi:
     def __init__(self):
         self.key_KL1 = [None] * 9
@@ -17,31 +26,65 @@ class Kasumi:
 
 
     def set_key(self, master_key):
+        key       = [None] * 9
+        key_prime = [None] * 9
+
+        master_key_prime = master_key ^ 0x0123456789ABCDEFFEDCBA9876543210
+        for i in range(1, 9):
+            key[i]       = (master_key       >> (16 * (8 - i))) & 0xFFFF
+            key_prime[i] = (master_key_prime >> (16 * (8 - i))) & 0xFFFF
+
+        for i in range(1, 9):
+            self.key_KL1[i] = _rol16(key[_mod(i + 0)], 1)
+            self.key_KL2[i] =  key_prime[_mod(i + 2)]
+            self.key_KO1[i] = _rol16(key[_mod(i + 1)], 5)
+            self.key_KO2[i] = _rol16(key[_mod(i + 5)], 8)
+            self.key_KO3[i] = _rol16(key[_mod(i + 6)], 13)
+            self.key_KI1[i] =  key_prime[_mod(i + 4)]
+            self.key_KI2[i] =  key_prime[_mod(i + 3)]
+            self.key_KI3[i] =  key_prime[_mod(i + 7)]
+
+
+    def fun_FI(self, ):
         pass
 
 
-    def fun_FI():
+    def fun_FO(self):
         pass
 
 
-    def fun_FO():
+    def fun_FL(self, ):
         pass
 
 
-    def fun_FL():
-        pass
+    def fun_f(self, input, round_i):
+        assert _bitlen(input)  <= 32
+        assert round_i >= 1 and round_i <= 8
+        if round_i % 2 == 1:
+            state  = self.fun_FL(input, round_i)
+            output = self.fun_FO(state, round_i)
+        else:
+            state  = self.fun_FO(input, round_i)
+            output = self.fun_FL(state, round_i)
+        return output
 
 
-    def fun_f(input, round_i):
-        pass
+    def enc_1r(self, in_left, in_right, round_i):
+        assert _bitlen(in_left)  <= 32
+        assert _bitlen(in_right) <= 32
+        assert round_i >= 1 and round_i <= 8
+        out_right = in_left # note this is different from normal Feistel
+        out_left  = self.fun_f(in_left, round_i) ^ in_right
+        return out_left, out_right
 
 
-    def enc_1r(in_left, in_right, round_i):
-        pass
-
-
-    def dec_1r(in_left, in_right, round_i):
-        pass
+    def dec_1r(self, in_left, in_right, round_i):
+        assert _bitlen(in_left)  <= 32
+        assert _bitlen(in_right) <= 32
+        assert round_i >= 1 and round_i <= 8
+        out_left  = in_right
+        out_right = self.fun_f(in_right, round_i) ^ in_left
+        return out_left, out_right
 
 
     def enc(self, plaintext):
@@ -51,6 +94,7 @@ class Kasumi:
         for i in range(1, 9):
             left, right = self.enc_1r(left, right, i)
         return (left << 32) | right
+
 
     def dec(self, ciphertext):
         assert _bitlen(ciphertext) <= 64
